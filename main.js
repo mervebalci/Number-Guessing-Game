54f7f1450148192b97b5f93c445f2726ec0f0b40
Merve Balci <mervebalci090@gmail.com>
2023-03-03 17:39:54 -0400

    Adding if statement

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 document.querySelector('button').addEventListener('click', () => {
     let userInput = document.getElementById('input').value
 
-    let message = `<p>You guessed ${userInput}</p>`
+    // Showing user the number they guessed
+    // alert(`You guessed ${userInput}!`)
+
+    // OR another way
+    // let message = `<p>You guessed ${userInput}!</p>`
+    // let div = document.getElementById('user')
+    // div.insertAdjacentHTML('beforeend', message)
+
+
+    function score(userInput) {
+        let target = 32
+        if (userInput > target) {
+            let high = "Go lower..."
+            return high
+        } if (userInput < target) {
+            let low = "Go higher..."
+            return low
+        } if (userInput === target) {
+            return "Congratulations! You found it."
+        }
+    }
+    // Calling the function
+    let answer = score(userInput)
+
+    // Showing user the number they guessed and tell them go higher or lower till they find the target number
+    let message = `<p>You guessed ${userInput}! ${answer}</p>`
     let div = document.getElementById('user')
     div.insertAdjacentHTML('beforeend', message)
 })
